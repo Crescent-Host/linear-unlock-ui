578910c69b5d76ace0495f63d553c94062b9cfac
Crescent-Host <crescenthost098@gmail.com>
2026-01-15 09:45:59 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,34 +1,60 @@
 import type { AppProps } from "next/app";
-import { useEffect, useState } from "react";
-import { ethers } from "ethers";
-import Web3Modal from "web3modal";
-
-import { Box, Experimental_CssVarsProvider } from "@mui/material";
-import Layout from "components/Layout";
 import * as theming from "custom_theme";
-import "styles/globals.scss";
 
-let web3Modal: Web3Modal | null = null;
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/react";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { base, baseGoerli } from "wagmi/chains";
+import { Box, Experimental_CssVarsProvider } from "@mui/material";
+import "styles/globals.scss";
+import Layout from "components/Layout";
+
+const chains = [base, baseGoerli];
+const projectId = "83fde4ab80cf5b97cff4927c19d25825";
+
+const { publicClient } = configureChains(chains, [
+  w3mProvider({ projectId }),
+]);
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({
+    projectId,
+    chains,
+  }),
+  publicClient,
+});
+
+const ethereumClient = new EthereumClient(wagmiConfig, chains);
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [provider, setProvider] = useState<ethers.providers.Web3Provider | null>(null);
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      web3Modal = new Web3Modal({
-        cacheProvider: true,
-        providerOptions: {}
-      });
-    }
-  }, []);
-
   return (
-    <Experimental_CssVarsProvider theme={theming.customTheme} defaultMode="dark">
-      <Box sx={{ bgcolor: "darkPurple.main" }} height="100%" width="100%">
-        <Layout provider={provider} setProvider={setProvider} web3Modal={web3Modal}>
-          <Component {...pageProps} />
-        </Layout>
-      </Box>
+    <Experimental_CssVarsProvider
+      theme={theming.customTheme}
+      defaultMode="dark"
+    >
+      <WagmiConfig config={wagmiConfig}>
+        <Box sx={{ bgcolor: "darkPurple.main" }} minHeight="100vh">
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </Box>
+      </WagmiConfig>
+
+      <Web3Modal
+        projectId={projectId}
+        ethereumClient={ethereumClient}
+        themeMode="dark"
+        themeVariables={{
+          "--w3m-font-family": "Roboto, sans-serif",
+          "--w3m-accent-color": "#730DE7",
+          "--w3m-background-color": "#2e1d4b",
+        }}
+      />
     </Experimental_CssVarsProvider>
   );
 }
